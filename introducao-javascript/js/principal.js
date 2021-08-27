004965122a0b4380788e24a929e5c6b71404a1e7
zetolenttinopc <zetolenttino@gmail.com>
2021-08-27 11:57:13 -0300

    calculando imc

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -11,9 +11,22 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
+var pesoEhValido = true;
+var alturaEhValida = true;
+
 if(peso < 0 || peso > 1000) {
-    console.log("peso inválido");
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso invalido!"
 }
 
+if(altura < 0 || altura > 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValido = false;
+    tdImc.textContent = "Altura invalida!"
+}
+
+if(alturaEhValida && pesoEhValido){
 var imc = peso / (altura * altura);
 tdImc.textContent = imc;
+}
